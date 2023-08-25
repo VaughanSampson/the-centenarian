18e19dbb18d08b3e737c4d4382abed143c5c1825
TimurRogan <128713660+VaughanSampson@users.noreply.github.com>
2023-08-25 22:34:27 +1000

    Update ArduinoInputTestCube.cs
    Smooth attempt 1.

--- Assets/Input/ArduinoInputTestCube.cs
+++ Assets/Input/ArduinoInputTestCube.cs
@@ -4,14 +4,39 @@ using UnityEngine;
 
 public class ArduinoInputTestCube : MonoBehaviour
 {
+    [SerializeField] private float velocity;
+    [SerializeField] private float y;
+    [SerializeField] private int[] inputList;
+    [SerializeField] private int inputListIndex;
+
     // Start is called before the first frame update
     void Start()
     {
-        ArduinoInput.GetSingleInput += SetY;
+        ArduinoInput.GetSingleInput += GetInput;
+
+        inputList = new int[] {0,0,0,0,0};
     }
 
-    public void SetY(int y) {
-        transform.position = new Vector3(0, y/60, 0);
+    public void Update()
+    {
+    }
+
+    public void GetInput(int input) {
+        if (input > 60)
+            return;
+
+        inputList[inputListIndex] = input;
+        inputListIndex++;
+        if (inputListIndex > inputList.Length)
+            inputListIndex = 0;
+
+        int y = 0;
+
+        foreach (int i in inputList)
+            y += i;
+        y /= inputList.Length;
+
+        transform.position = new Vector3(0, y, 0);
     }
 
 }
