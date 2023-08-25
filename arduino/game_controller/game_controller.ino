b5b3f6ded2943f1a48ad727a0a51c0a425137e89
mustaqim331 <mustaqim.proton@gmail.com>
2023-08-25 23:07:56 +1000

    Update game_controller.ino

--- arduino/game_controller/game_controller.ino
+++ arduino/game_controller/game_controller.ino
@@ -1,28 +1,41 @@
 const int trigPin1 = 8; // Trigger Pin of Ultrasonic Sensor
 const int echoPin1 = 9; // Echo Pin of Ultrasonic Sensor
 
-long duration1;
-int distance1;
+//const int trigPin2 = 10; // Trigger Pin of Ultrasonic Sensor
+//const int echoPin2 = 11; // Echo Pin of Ultrasonic Sensor
+
+long duration1, duration2;
+int distance1, distance2;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(trigPin1, OUTPUT);
   pinMode(echoPin1, INPUT);
+  //pinMode(trigPin2, OUTPUT);
+  //pinMode(echoPin2, INPUT);
   Serial.begin(9600); // Starting Serial Terminal
 }
 
 void loop() {
   // put your main code here, to run repeatedly:  
   digitalWrite(trigPin1, LOW);
+  //digitalWrite(trigPin2, LOW);
   delayMicroseconds(5);
   digitalWrite(trigPin1, HIGH);
+  //digitalWrite(trigPin2, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin1, LOW);
+  //digitalWrite(trigPin2, LOW);
 
   duration1 = pulseIn(echoPin1, HIGH);
+  //duration2 = pulseIn(echoPin2, HIGH);
 
   distance1 = duration1 * 0.034 / 2;
+  //distance2 = duration2 * 0.034 / 2;
 
+  //Serial.print("distance1: ");
   Serial.println(distance1);
+  //Serial.print("distance2: ");
+  //Serial.println(distance2);
   
 }
