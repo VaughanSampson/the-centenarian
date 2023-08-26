8e5450ac104ec0c5244f0f8db328bca3c208770a
mustaqim331 <mustaqim.proton@gmail.com>
2023-08-27 09:20:15 +1000

    add new pause button

--- arduino/game_controller/game_controller.ino
+++ arduino/game_controller/game_controller.ino
@@ -1,6 +1,7 @@
 const int trigPin1 = 8; // Trigger Pin of Ultrasonic Sensor
 const int echoPin1 = 9; // Echo Pin of Ultrasonic Sensor
 const int buttonPin = 10;  // Push button pin number
+const int pausePin = 11;  // Push button pin number
 //const int ledPin = LED_BUILTIN;    // the number of the LED pin for testing purpose
 const int potPin = A0; //Potentiometer pin number
 
@@ -8,7 +9,7 @@ long duration1;
 int distance1;
 
 int buttonState = 0;  // variable for reading the pushbutton status
-
+int pauseState = 1; // variable for reading the pause button
 
 void setup() {
   // put your setup code here, to run once:
@@ -16,6 +17,7 @@ void setup() {
   pinMode(trigPin1, OUTPUT);
   pinMode(echoPin1, INPUT);
   pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(pausePin, INPUT_PULLUP);
 }
 
 void loop() {
@@ -23,6 +25,7 @@ void loop() {
 
   // read the state of the pushbutton and potentiometer value:
   buttonState = digitalRead(buttonPin);
+  pauseState = digitalRead(pausePin);
   int val = analogRead(potPin);
 
   digitalWrite(trigPin1, LOW);
@@ -41,7 +44,8 @@ void loop() {
   Serial.println(buttonState);
   Serial.print("G");
   Serial.println(val);
-  
+  Serial.print("P");
+  Serial.println(pauseState);
 
   //Serial.println("");
   //Serial.write(distVal);
