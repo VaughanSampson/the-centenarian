d2eff0617169f7bc405aec52e775f32cc64e07b6
BeriaL-CN <40686646+BeriaL-CN@users.noreply.github.com>
2023-08-26 12:01:29 +1000

    StartMenu Button Functions Update

---
+++ Assets/Scripts/StartMenu/QuitButtonHandle.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitButtonHandle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
