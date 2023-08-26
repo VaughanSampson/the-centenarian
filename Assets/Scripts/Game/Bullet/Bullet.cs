38fb46dddad9694b0b7efc317aa1dbb04da8b142
BeriaL-CN <40686646+BeriaL-CN@users.noreply.github.com>
2023-08-26 14:58:53 +1000

    bullet function update

---
+++ Assets/Scripts/Game/Bullet/Bullet.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] protected float speed = 1000f;
+    [SerializeField] private Rigidbody2D rigidBody;
+
+    public void Initiate()
+    {
+        //transform.Translate(new Vector2(0, speed * Time.deltaTime));
+        rigidBody.AddForce(transform.up * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Enemy")
+        {
+            return; 
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnBecameInvisible()
+    {
+        if (this.enabled)
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+}
---
+++ Assets/Scripts/Game/EnemyTestBullet.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTestxBullet : MonoBehaviour
+{
+    public Bullet BulletPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButton(0))
+        {
+            Bullet bullet = Instantiate(BulletPrefab, this.transform.position, this.transform.rotation);
+            bullet.Initiate();
+        }
+    }
+}
